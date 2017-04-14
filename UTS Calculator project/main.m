6e779ca112f463d550aa027044056a576b975431
justjamesqq <James46468282@gmail.com>
2017-04-14 17:55:43 +1000

    More roadblocks in place to fix potential errors (white spacing)

--- UTS Calculator project/main.m
+++ UTS Calculator project/main.m
@@ -10,11 +10,17 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        int result = 0; //Value to store the answer
         char str[50] = {0}; //Initializing the array that will contain the equation string
+        
         printf("Enter your equation prefixed with a ./cal\n"); // user prompt
         scanf("%s",str); //read in the input and put it into the array of characters "str"
         
-        NSString *equation = [NSString stringWithUTF8String:str]; //Convert the str array to a NSString object
+        NSString *equation = [NSString stringWithUTF8String:str]; //Convert the str array to a NSString object so that we can trim it
+        equation = [equation stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]; //Trim both the front and the back of the equation to remove any white spaces
+        
+        NSArray *splitup = [equation componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]; //Split up the NSString into individual characters put into an array to get rid of the potential white space in between characters so that we can evaluate each character one by one
+        
         NSLog(@"This is working %@", equation); //Attempted to print Object
     }
     return 0;
