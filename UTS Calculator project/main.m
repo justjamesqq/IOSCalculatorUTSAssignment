180f32c544df6e555d3f3ba3e38e32d5f76ca138
justjamesqq <James46468282@gmail.com>
2017-04-14 16:57:23 +1000

    Initial Commit

---
+++ UTS Calculator project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UTS Calculator project
+//
+//  Created by James Lin on 14/4/17.
+//  Copyright © 2017 James Lin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
